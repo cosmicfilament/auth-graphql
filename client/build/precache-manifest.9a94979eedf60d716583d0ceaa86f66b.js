496feccbb79fba78a70b1971527662a800961a48
cosmicfilament <john@thecosmicfilament.com>
2020-04-29 14:36:34 -0400

    tweaks on the prod build process

---
+++ client/build/precache-manifest.9a94979eedf60d716583d0ceaa86f66b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd07ac361ab21a15806bb0611dd072bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e135f33f2691d5fa7782",
+    "url": "/static/css/2.55548263.chunk.css"
+  },
+  {
+    "revision": "1213d1257ddee9250a6c",
+    "url": "/static/css/main.9f923647.chunk.css"
+  },
+  {
+    "revision": "e135f33f2691d5fa7782",
+    "url": "/static/js/2.c6200bf5.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.c6200bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1213d1257ddee9250a6c",
+    "url": "/static/js/main.55b1fe2b.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  },
+  {
+    "revision": "6ec7e125fba218e922e1b72d3cf556e9",
+    "url": "/static/media/dashboard.6ec7e125.png"
+  },
+  {
+    "revision": "8a3a568bdae818a4a51219badc6b7c7f",
+    "url": "/static/media/sandpiper.8a3a568b.png"
+  }
+]);
